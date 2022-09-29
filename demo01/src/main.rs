4d1229323ef72c2c2f4ffa5396db1a5253013108
xuyujian <xuyujian@cbib.com>
2022-09-29 14:03:13 +0800

    feat: add demo01

---
+++ demo01/src/main.rs
@@ -0,0 +1,16 @@
+use std::fs;
+
+fn main() {
+  let url = "http://www.rust-lang.org/";
+  let output = "rust.md";
+  println!("Fetching url: {}", url);
+  let body = reqwest::blocking::get(url).unwrap().text().unwrap();
+  
+  println!("Converting html to markdown。。。");
+
+  let md = html2md::parse_html(&body);
+
+  fs::write(output, md.as_bytes()).unwrap();
+
+  println!("Converting markdown has benn saved in {}", output);
+}
