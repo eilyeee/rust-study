759359888e3624947e56c4fe799a88642399c810
xuyujian <xuyujian@cbib.com>
2022-09-29 16:03:42 +0800

    feat: return Result

--- demo01/src/main.rs
+++ demo01/src/main.rs
@@ -2,18 +2,20 @@ pub mod fib;
 
 use std::fs;
 
-fn main() {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
   let url = "http://www.rust-lang.org/";
   let output = "rust.md";
   println!("Fetching url: {}", url);
-  let body = reqwest::blocking::get(url).unwrap().text().unwrap();
+  let body = reqwest::blocking::get(url)?.text()?;
   
   println!("Converting html to markdown。。。");
 
   let md = html2md::parse_html(&body);
 
-  fs::write(output, md.as_bytes()).unwrap();
+  fs::write(output, md.as_bytes())?;
 
   println!("Converting markdown has benn saved in {}", output);
   fib::main();
+
+  Ok(())
 }
